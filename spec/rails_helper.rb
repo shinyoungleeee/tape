df799c23979f96487d2cb379a749a4a010ee74bc
Shinyoung Lee <shinyounglee.315@gmail.com>
2017-05-05 20:55:47 -0400

    pass tests

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -55,6 +55,7 @@ RSpec.configure do |config|
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
   config.include FactoryGirl::Syntax::Methods
+  config.include Devise::Test::ControllerHelpers, type: :controller
 end
 
 Shoulda::Matchers.configure do |config|

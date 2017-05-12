c52c6e152c48f8fc3eecbcb31ebda8f1c9085daf
Shinyoung Lee <shinyounglee.315@gmail.com>
2017-05-12 17:37:58 -0400

    create group show pages

--- app/controllers/api/v1/groups_controller.rb
+++ app/controllers/api/v1/groups_controller.rb
@@ -5,7 +5,16 @@ class Api::V1::GroupsController < ApiController
     render json: Group.all
   end
 
+  def show
+    render json: Group.find(params[:id])
+  end
+
   def join
 
+  end
+
+  def albums
+    this_group = Group.find(params[:group_id])
+    render json: this_group.albums
   end
 end

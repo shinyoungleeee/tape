cbbfcab4cd4bb12315c304b0e172716208616146
Shinyoung Lee <shinyounglee.315@gmail.com>
2017-05-12 18:05:24 -0400

    autogenerate user handle

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,6 +11,7 @@ class User < ApplicationRecord
       user.password = Devise.friendly_token[0,20]
       user.name = auth.info.name   # assuming the user model has a name
       user.image = auth.info.image # assuming the user model has an image
+      user.handle = auth.info.name
       # If you are using confirmable and the provider(s) you use validate emails,
       # uncomment the line below to skip the confirmation emails.
       # user.skip_confirmation!

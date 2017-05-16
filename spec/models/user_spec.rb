38bfd1c8b7095b3ccfc3fe71c67d5ef47758cbe6
Shinyoung Lee <shinyounglee.315@gmail.com>
2017-05-16 13:39:46 -0400

    improve user model tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject { create(:user) }
+  it { should validate_presence_of(:name) }
+  it { should validate_presence_of(:handle) }
+  it { should validate_presence_of(:email) }
+  it { should validate_presence_of(:password) }
+  it { should have_many(:album_likes) }
+  it { should have_many(:user_groups) }
+  it { should have_many(:groups) }
+  it { should have_many(:created_groups) }
 end

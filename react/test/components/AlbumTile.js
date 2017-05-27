fd91e1aa9b9ed3fd322639d07df7a090e0d9ca81
Shinyoung Lee <shinyounglee.315@gmail.com>
2017-05-27 14:12:50 -0400

    add react test

--- react/test/components/AlbumTile.js
+++ react/test/components/AlbumTile.js
@@ -1,9 +1,10 @@
 import AlbumTile from 'components/AlbumTile'
 
 describe('AlbumTile', () => {
-  let key, id, name, art, year, kind, links, wrapper;
+  let key, id, name, art, year, kind, artists, links, likeButton, likeCount, clickLikeHandler, wrapper;
 
   beforeEach(() => {
+    clickLikeHandler = jasmine.createSpy('clickLikeHandler spy')
     wrapper = mount(
       <AlbumTile
         key={1}
@@ -12,11 +13,18 @@ describe('AlbumTile', () => {
         art={'#'}
         year={2017}
         kind={'album'}
+        artists={[{
+          id: 1,
+          name: "Artist"
+        }]}
         links={[{
           id: 1,
           service: 'Spotify',
           url: 'https://open.spotify.com/album/2ANVost0y2y52ema1E9xAZ'
         }]}
+        likeButton={"liked"}
+        likeCount={1}
+        clickLikeHandler={clickLikeHandler}
       />
     )
   })
@@ -24,5 +32,12 @@ describe('AlbumTile', () => {
   it('should render a series of divs', () => {
     expect(wrapper.find('div.column.column-block')).toBePresent();
     expect(wrapper.find('div.album-container')).toBePresent();
+    expect(wrapper.find('div.album-overlay')).toBePresent();
+    expect(wrapper.find('div.album-text.flex-container.align-center.align-middle')).toBePresent();
   });
+
+  it('should invoke the clickLikeHandler function from props when clicked', () => {
+    wrapper.find('img.like-button').simulate('click');
+    expect(clickLikeHandler).toHaveBeenCalled();
+  })
 });
